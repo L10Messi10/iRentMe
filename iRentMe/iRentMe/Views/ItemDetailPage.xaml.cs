ffd1b4e4902311a6d490935321f078eb82f1b3d1
L10Messi10 <loveyourenemy@outlook.com>
2020-11-18 09:15:17 +0800

    Add project files.

---
+++ iRentMe/iRentMe/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using iRentMe.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace iRentMe.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ iRentMe/iRentMe/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using iRentMe.Models;
+using iRentMe.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iRentMe.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
