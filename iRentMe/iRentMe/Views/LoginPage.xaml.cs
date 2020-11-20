5f68062439ed14d9dba28169fadaefcdee1773d8
L10Messi10 <loveyourenemy@outlook.com>
2020-11-20 15:15:47 +0800

    improvements

--- iRentMe/iRentMe/Views/LoginPage.xaml.cs
+++ iRentMe/iRentMe/Views/LoginPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +17,12 @@ namespace iRentMe.Views
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
+        }
+        public ICommand ForgotPasswordCommand => new Command(OnSignUp);
+
+        private async void OnSignUp()
+        {
+            await Navigation.PushAsync(new SignUpPage());
         }
     }
 }
