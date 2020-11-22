f232fcee939e9a2729331d02d725cc583217ad24
L10Messi10 <loveyourenemy@outlook.com>
2020-11-22 08:43:12 +0800

    improvements

--- iRentMe/iRentMe.Android/MainActivity.cs
+++ iRentMe/iRentMe.Android/MainActivity.cs
@@ -18,7 +18,7 @@ namespace iRentMe.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
--- iRentMe/iRentMe/Views/SignUpPage.xaml.cs
+++ iRentMe/iRentMe/Views/SignUpPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,12 @@ namespace iRentMe.Views
         public SignUpPage()
         {
             InitializeComponent();
+        }
+        public ICommand OnSignInCommand => new Command(OnSignIn);
+
+        private async void OnSignIn()
+        {
+            await Navigation.PushAsync(new LoginPage());
         }
     }
 }
