acc535f84d90555ffaa3237168b4995cce2db373
L10Messi10 <loveyourenemy@outlook.com>
2021-04-16 16:21:26 +0800

    impovements

--- iRentMe/iRentMe.Android/MainActivity.cs
+++ iRentMe/iRentMe.Android/MainActivity.cs
@@ -26,7 +26,6 @@ namespace iRentMe.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
